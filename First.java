e8dff09d4fe5336ca3583fa426cd81591d847b64
PoviArasu <94035032+PoviArasu@users.noreply.github.com>
2025-03-18 20:53:02 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,11 @@
+import java.io.*; //printstream
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow brotha");
+		System.out.print("Hellow brotha");//syntax
+	}
+
+}
