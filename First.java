fe5fa0d746c59701ebc21cad1deb7c6e7dddd886
PoviArasu <94035032+PoviArasu@users.noreply.github.com>
2025-03-19 09:07:30 +0530

    Update First.java
    added data about strings and other things

--- First.java
+++ First.java
@@ -1,11 +1,35 @@
+package practice;
 import java.io.*; //printstream
 
 public class First {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hellow brotha");
-		System.out.print("Hellow brotha");//syntax
-	}
+		int ticketPrice=900;//Camel Case
+		System.out.println(ticketPrice);
+		
+//		double p= 22.4;
+//		String p1= "lamine yamal";
+//		String p2= "lamine yamalie";
+////	String p2= new String ("lamine yamal");
+////	String p3= new String ("lamine yampl");
+//////		strin p3= p1==p2;
+////		System.out.println("hello"+123);
+////		System.out.println(p);
+////		System.out.println("Result :"+(10+20));
+////		System.out.println((p1.equals(p2)));
+////		System.out.println(p2==p3);
+////		System.out.println(p2.hashCode()+" "+p3.hashCode());
+//		System.out.println(p1);
+//		
+//		
+//	int a=100;	
+//	System.out.println(a);
+//	    a=120;
+//	System.out.println(a);
+//	
+//	String k7= "kumar";
+//	System.out.println(k7);
+//		k7="jamal";
+//		System.out.println(k7);
+		}
 
 }
